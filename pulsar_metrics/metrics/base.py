35d2c4d8faf16ea7e3688ce14bde004e060a4e39
Adel Benlagra <abenlagra@rocketscience.one>
2022-07-25 00:13:04 -0400

    base metrics class and results data structure

---
+++ pulsar_metrics/metrics/base.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+import pandas as pd
+from pydantic import BaseModel, validator
+
+
+class MetricResults(BaseModel):
+
+    """Data structure for the results of a metric"""
+
+    name: str = None
+    model_id: str = None
+    model_version: int = None
+    data_id: str = None
+    value: float = None
+    status: bool = None
+    period_start: datetime = None
+    period_end: datetime = None
+    timestamp: datetime = datetime.now()
+    conf_int: list = None
+
+    @validator("timestamp", always=True)
+    def timestamp_later_than_period_end(cls, v, values, **kwargs):
+        if v < values["period_end"]:
+            raise ValueError("Current timestamp earlier than period end")
+        return v
+
+
+class AbstractMetrics(ABC):
+
+    """Base abstract class for metrics
+
+    Parameters
+    ----------
+    - name: name of the metrics
+    - data: dataset from which the metric is calculated
+    """
+
+    def __init__(self, name: str, data: pd.DataFrame):
+
+        self._name = name
+        self._data = data.copy(deep=True)  # Not sure I want to attach the data as an attribute ...
+
+        # TODO: validation on the dataset ?
+
+        try:
+            self._model_id = str(data["model_id"].unique()[0])
+            self._model_version = str(data["model_version"].unique()[0])
+            self._period_start = data.date.min()
+            self._period_end = data.date.max()
+        except Exception as e:
+            print(str(e))
+
+    @property
+    @abstractmethod
+    def evaluate(self) -> MetricResults:
+        raise NotImplementedError
