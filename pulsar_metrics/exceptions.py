aecd418e11b3af15d46e7c09d4cd5625a22f916a
Jananjoy_rk <jrajkumar@rocketscience.one>
2023-04-11 12:17:25 -0400

    Update exceptions.py

--- pulsar_metrics/exceptions.py
+++ pulsar_metrics/exceptions.py
@@ -2,6 +2,7 @@ class CustomExceptionPulsarMetric(Exception):
     """
     Custom error handler that is raised in Pulsar Metrics(with value and message fields)
     """
+
     def __init__(self, value: str, message: str) -> None:
         self.value = value
         self.message = message
