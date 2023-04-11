85ed86ab1563f66329147422639e04f765963534
Jananjoy_rk <jrajkumar@rocketscience.one>
2023-04-11 11:39:46 -0400

    Update utils.py

--- pulsar_metrics/utils.py
+++ pulsar_metrics/utils.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 from .exceptions import CustomExceptionPulsarMetric as error_msg
 
-ERROR_MSG_VECTOR_THRESHOLD="Vector Threshold should have only two distinct elements [Min,Max]"
-ERROR_MSG_MISSING_KEY="Missing key for the column in the dataset"
+ERROR_MSG_VECTOR_THRESHOLD = "Vector Threshold should have only two distinct elements [Min,Max]"
+ERROR_MSG_MISSING_KEY = "Missing key for the column in the dataset"
 
 
 def validate_dataframe(data: pd.DataFrame, y_name: str = "y_true", pred_name: str = "y_pred"):
@@ -14,23 +14,22 @@ def validate_dataframe(data: pd.DataFrame, y_name: str = "y_true", pred_name: st
         raise error_msg(
             value=y_name,
             message=ERROR_MSG_MISSING_KEY,
-            )
-
+        )
     elif pred_name not in data.columns:
         raise error_msg(
             value=pred_name,
-            message= ERROR_MSG_MISSING_KEY,
-            ) 
+            message=ERROR_MSG_MISSING_KEY,
+        )
     elif "date" not in data.columns:
         raise error_msg(
-            value=date,
+            value=None,
             message=ERROR_MSG_MISSING_KEY,
-            ) 
+        )
     elif "model_id" not in data.columns:
         raise error_msg(
-            value=model_id,
+            value=None,
             message=ERROR_MSG_MISSING_KEY,
-            ) 
+        )
     else:
         print("Input dataframe is validated in validate_dataframe()!")
         return True
@@ -42,14 +41,14 @@ def compare_to_threshold(value: float, threshold: Union[list, float, int], upper
 
     try:
         if isinstance(threshold, (float, int)):
-            status  = value < threshold if upper_bound else threshold < value
+            status = value < threshold if upper_bound else threshold < value
         elif isinstance(threshold, list):
-            status  = True if (len(threshold)==2) and  len(set())==len(threshold) else False
+            status = True if (len(threshold) == 2) and len(set()) == len(threshold) else False
         else:
             raise error_msg(
                 value=threshold,
                 message=ERROR_MSG_VECTOR_THRESHOLD,
-                )
+            )
         return status
     except Exception as e:
         print(f"Exception caught for ValueError in compare_to_threshold():: {str(e)}")
