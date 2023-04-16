14a67f5707e1b27b2dd2862fd317ab634f0f27de
Adel Benlagra <abenlagra@rocketscience.one>
2023-04-16 13:42:02 -0400

    formatting and linting

--- pulsar_metrics/utils.py
+++ pulsar_metrics/utils.py
@@ -1,7 +1,7 @@
+from numbers import Number
 from typing import Union
 
 import pandas as pd
-from numbers import Number
 
 from .exceptions import CustomExceptionPulsarMetric as error_msg
 
@@ -10,7 +10,6 @@ ERROR_MSG_MISSING_KEY = "Missing key for the column in the dataset"
 
 
 def validate_dataframe(data: pd.DataFrame, y_name: str = "y_true", pred_name: str = "y_pred"):
-
     if y_name not in data.columns:
         raise error_msg(
             value=y_name,
@@ -37,13 +36,12 @@ def validate_dataframe(data: pd.DataFrame, y_name: str = "y_true", pred_name: st
 
 
 def compare_to_threshold(value: float, threshold: Union[list, Number], upper_bound=True):
-
     status = None
 
     if isinstance(threshold, Number):
         status = value < threshold if upper_bound else threshold < value
     elif isinstance(threshold, list) and (len(set(threshold)) == 2) and all(isinstance(i, Number) for i in threshold):
-        status = True if  (min(threshold) < value < max(threshold)) else False
+        status = True if (min(threshold) < value < max(threshold)) else False
     else:
         raise ValueError(ERROR_MSG_VECTOR_THRESHOLD)
 
